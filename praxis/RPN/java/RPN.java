ccbed2c64678e8244f0a5859910e3f4f799e38e3
Colin Williams <lackita@gmail.com>
2013-07-17 10:16:42 -0400

    copied calculations to Token class

--- praxis/RPN/java/RPN.java
+++ praxis/RPN/java/RPN.java
@@ -1,9 +1,54 @@
+import java.util.*;
+
 class RPN {
 	public int calc(String expression) {
-		String[] tokens = expression.split(" ");
-		if (tokens.length == 1) {
-			return Integer.parseInt(tokens[0]);
+		Queue<String> tokens = tokens(expression);
+		if (tokens.size() == 1) {
+			return Integer.parseInt(tokens.poll());
 		}
-		return Integer.parseInt(tokens[0]) + Integer.parseInt(tokens[1]);
+
+		int first = Integer.parseInt(tokens.poll());
+		int second = Integer.parseInt(tokens.poll());
+		String operator = tokens.poll();
+
+		Token operator_token = Token.build(operator);
+		if (operator.equals("+")) {
+			return first + second;
+		}
+		else if (operator.equals("-")) {
+			return first - second;
+		}
+		return 0;
+	}
+
+	private Queue<String> tokens(String expression) {
+		Queue<String> tokens = new ArrayDeque<String>();
+
+		for (String token : expression.split(" ")) {
+			tokens.add(token);
+		}
+
+		return tokens;
+	}
+}
+
+class Token {
+	public static Token build(String descriptor) {
+		return new Token(descriptor);
+	}
+
+	private String descriptor;
+	public Token(String descriptor) {
+		this.descriptor = descriptor;
+	}
+
+	public int calculate(int first, int second) {
+		if (descriptor.equals("+")) {
+			return first + second;
+		}
+		else if (descriptor.equals("-")) {
+			return first - second;
+		}
+		return 0;
 	}
 }
