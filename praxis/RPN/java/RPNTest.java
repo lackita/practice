c71fb74ca849e76eb74d162248d436b435d1114b
Colin Williams <lackita@gmail.com>
2013-07-17 19:17:35 -0400

    Simplified main loop

--- praxis/RPN/java/RPNTest.java
+++ praxis/RPN/java/RPNTest.java
@@ -10,9 +10,35 @@ public class RPNTest {
 	}
 
 	@Test
-	public void Plus() {
+	public void Addition() {
 		RPN rpn = new RPN();
 		assertEquals(0, rpn.calc("0 0 +"));
 		assertEquals(1, rpn.calc("0 1 +"));
+	}
+
+	@Test
+	public void Subtraction() {
+		RPN rpn = new RPN();
+		assertEquals(0, rpn.calc("0 0 -"));
+		assertEquals(1, rpn.calc("1 0 -"));
+		assertEquals(1, rpn.calc("2 1 -"));
+	}
+
+	@Test
+	public void Multiplication() {
+		RPN rpn = new RPN();
+		assertEquals(0, rpn.calc("0 1 *"));
+	}
+
+	@Test
+	public void Division() {
+		RPN rpn = new RPN();
+		assertEquals(2, rpn.calc("4 2 /"));
+	}
+
+	@Test
+	public void Composite() {
+		RPN rpn = new RPN();
+		assertEquals(5, rpn.calc("1 2 2 + +"));
 	}
 }
