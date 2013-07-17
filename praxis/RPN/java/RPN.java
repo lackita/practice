5dcc8d21580d99cf8d1b524ccf95d091c26136d9
Colin Williams <lackita@gmail.com>
2013-07-17 19:17:30 -0400

    Simplified main loop

--- praxis/RPN/java/RPN.java
+++ praxis/RPN/java/RPN.java
@@ -2,53 +2,86 @@ import java.util.*;
 
 class RPN {
 	public int calc(String expression) {
-		Queue<String> tokens = tokens(expression);
-		if (tokens.size() == 1) {
-			return Integer.parseInt(tokens.poll());
-		}
-
-		int first = Integer.parseInt(tokens.poll());
-		int second = Integer.parseInt(tokens.poll());
-		String operator = tokens.poll();
-
-		Token operator_token = Token.build(operator);
-		if (operator.equals("+")) {
-			return first + second;
-		}
-		else if (operator.equals("-")) {
-			return first - second;
-		}
-		return 0;
+		Stack<Token> operands = operands(expression);
+		return operands.pop().calculate(operands);
 	}
 
-	private Queue<String> tokens(String expression) {
-		Queue<String> tokens = new ArrayDeque<String>();
+	private Stack<Token> operands(String expression) {
+		Stack<Token> operands = new Stack<Token>();
 
 		for (String token : expression.split(" ")) {
-			tokens.add(token);
+			operands.push(build_token(token));
 		}
 
-		return tokens;
+		return operands;
+	}
+
+	public Token build_token(String descriptor) {
+		Token t;
+		switch (descriptor) {
+		case "+": t = new Addition();
+			break;
+		case "-": t = new Subtraction();
+			break;
+		case "*": t = new Multiplication();
+			break;
+		case "/": t = new Division();
+			break;
+		default: t = new Number(descriptor);
+			break;
+		}
+		return t;
 	}
 }
 
-class Token {
-	public static Token build(String descriptor) {
-		return new Token(descriptor);
+abstract class Token {
+	abstract public int calculate(Stack<Token> operands);
+
+	public boolean isOperator() {
+		return true;
+	}
+}
+
+class Addition extends Token {
+	public int calculate(Stack<Token> operands) {
+		return operands.pop().calculate(operands) + operands.pop().calculate(operands);
+	}
+}
+
+class Subtraction extends Token {
+	public int calculate(Stack<Token> operands) {
+		Token first = operands.pop();
+		Token second = operands.pop();
+		return second.calculate(operands) - first.calculate(operands);
+	}
+}
+
+class Multiplication extends Token {
+	public int calculate(Stack<Token> operands) {
+		return operands.pop().calculate(operands) * operands.pop().calculate(operands);
+	}
+}
+
+class Division extends Token {
+	public int calculate(Stack<Token> operands) {
+		Token first = operands.pop();
+		Token second = operands.pop();
+		return second.calculate(operands) / first.calculate(operands);
+	}
+}
+
+class Number extends Token {
+	private String value;
+	public Number(String value) {
+		super();
+		this.value = value;
 	}
 
-	private String descriptor;
-	public Token(String descriptor) {
-		this.descriptor = descriptor;
+	public int calculate(Stack<Token> operands) {
+		return Integer.parseInt(value);
 	}
 
-	public int calculate(int first, int second) {
-		if (descriptor.equals("+")) {
-			return first + second;
-		}
-		else if (descriptor.equals("-")) {
-			return first - second;
-		}
-		return 0;
+	public boolean isOperator() {
+		return false;
 	}
 }
